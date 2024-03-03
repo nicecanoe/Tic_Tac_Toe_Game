090163e5e85ee8345b536f58af0c4e10344e934d
yangweili <wangweilei1064@163.com>
2024-03-03 16:20:18 +0800

    complete the game of single mode and easy ai mode

---
+++ README.md
@@ -0,0 +1,21 @@
+#  Tic Tac Toe Game
+
+## the project will complete the list:
+
+- [x] two-player game
+- [ ] single game
+- [ ] the GUI
+
+## two-player game
+
+- make the number as 0~9 for the board
+- list the all possible for winning to a list
+- use X for red player, use ⭕ for blue player
+
+## single game
+
+- [x] easy_mode: randomly choose a position
+- [x] medium_mode: intercept the position which the player would win
+- [ ] hard_mode: attempts to win first, then blocks the player to win
+
+## the GUI
---
+++ main.py
@@ -0,0 +1,144 @@
+from ASCII_ART import logo
+import random
+import itertools
+
+the_game_array = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8],  # 行
+                [0, 3, 6], [1, 4, 7], [2, 5, 8],  # 列
+                [0, 4, 8], [2, 4, 6]]
+all_step = []
+the_x_step = []
+the_o_step = []
+
+# 目前棋局情况
+def print_board():
+    for i in range(3):
+        for j in range(3):
+            if j == 2:
+                print(the_game_array[i][j], end="")
+            else:
+                print(the_game_array[i][j], end=" | ")
+        print("")
+        print("----------")
+
+# 判定是否有一方获胜
+def judge_winner(str):
+    list = []
+    ordered_triplets = []
+    for i in range(3):
+        for j in range(3):
+            if the_game_array[i][j] == str:
+                list.append(i*3+j)
+    for i in range(len(list)):
+        for j in range(i + 1, len(list)):
+            for k in range(j + 1, len(list)):
+                ordered_triplets.append([list[i], list[j], list[k]])
+    for li in ordered_triplets:
+        if li in wins:
+            return True
+
+
+def easy_difficulty():
+    next_o_step = random.choice(all_step)
+    if len(all_step) < 9:
+        while next_o_step in all_step:
+            next_o_step = random.choice(range(9))
+        all_step.append(next_o_step)
+        i = int(next_o_step/3)
+        j = int(next_o_step)%3
+        print("computer choose")
+        the_game_array[i][j]= "⭕"
+
+
+def medium_difficulty():
+    go_on = False
+    next_o_step = ''
+    if len(the_x_step) > 1:
+        unique_pairs = list(itertools.combinations(the_x_step, 2))
+        for li in wins:
+            for pair in unique_pairs:
+                if pair[0] in li and pair[1] in li:
+                    the_step = [element for element in li if element not in pair][0]
+                    if the_step not in the_x_step and the_step not in the_o_step:
+                        next_o_step = the_step
+                        go_on = True
+    if go_on == False and (len(the_x_step)+len(the_o_step)<9):
+        next_o_step = random.choice(the_x_step)
+        while next_o_step in the_o_step or next_o_step in the_x_step:
+            next_o_step = random.choice(range(9))
+    if len(the_x_step)+len(the_o_step) < 9:
+        the_o_step.append(next_o_step)
+        i = int(next_o_step/3)
+        j = int(next_o_step)%3
+        print("computer choose")
+        the_game_array[i][j]= "⭕"
+
+
+
+
+
+
+print(logo)
+win_the_game = True
+
+print_board()
+the_game_mode = input("Would you like to play single or double game? (single/double):\n")
+if the_game_mode == "double":
+    while win_the_game:
+        the_num = input("Welcome to Tic Tac Toe, you are blue one, enter the row and column numbers separated by ,\n").split(",")
+        the_game_array[int(the_num[0])-1][int(the_num[1])-1] = "X"
+        all_step.append((int(the_num[0]) - 1) * 3 + (int(the_num[1]) - 1))
+        print_board()
+        the_num = input("Next person, you are red one, Please enter the row and column numbers separated by ,\n").split(",")
+        the_game_array[int(the_num[0])-1][int(the_num[1])-1] = "⭕"
+        all_step.append((int(the_num[0]) - 1) * 3 + (int(the_num[1]) - 1))
+        if judge_winner("X"):
+            print("the red one win!")
+            win_the_game = False
+        elif judge_winner("⭕"):
+            print("the blue one win!")
+            win_the_game = False
+        elif len(all_step) == 9:
+            print("a draw")
+            win_the_game = False
+elif the_game_mode == "single":
+    the_game_difficulty = input("What difficulty would you like to? (easy|/medium/hard)\n")
+    while win_the_game:
+        the_num = input(
+            "Welcome to Tic Tac Toe, you are blue one, enter the row and column numbers separated by ,\n").split(",")
+        the_game_array[int(the_num[0]) - 1][int(the_num[1]) - 1] = "X"
+        print_board()
+        if the_game_difficulty == "easy":
+            all_step.append((int(the_num[0])-1)*3 + (int(the_num[1])-1))
+            easy_difficulty()
+            print_board()
+            if judge_winner("X"):
+                print("You win!")
+                win_the_game = False
+            elif judge_winner("⭕"):
+                print("the computer win!")
+                win_the_game = False
+            elif len(all_step) == 9:
+                print("a draw")
+                win_the_game = False
+        if the_game_difficulty == "medium":
+            the_x_step.append((int(the_num[0])-1)*3 + (int(the_num[1])-1))
+            medium_difficulty()
+            print_board()
+            print(len(the_o_step)+len(the_x_step))
+            if judge_winner("X"):
+                print("You win!")
+                win_the_game = False
+            elif judge_winner("⭕"):
+                print("the computer win!")
+                win_the_game = False
+            elif (len(the_x_step)+len(the_o_step)) == 9:
+                print("a draw")
+                win_the_game = False
+
+
+
+
+
+
+
